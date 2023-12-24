f2ac213532357fcf5e38386c90848491c395f86b
Vo Ta Dung <47474729+votadung2000@users.noreply.github.com>
2023-12-24 14:50:42 +0700

    handle logic clean architecture list product

---
+++ modules/product/biz/list_product_biz.go
@@ -0,0 +1,38 @@
+package bizProduct
+
+import (
+	"context"
+	"fastFood/common"
+	modelProduct "fastFood/modules/product/model"
+)
+
+type ListProductStorage interface {
+	ListProduct(
+		ctx context.Context,
+		filter *modelProduct.Filter,
+		paging *common.Paging,
+		moreKeys ...string,
+	) ([]modelProduct.Product, error)
+}
+
+type listProductBiz struct {
+	store ListProductStorage
+}
+
+func ListProductBiz(store ListProductStorage) *listProductBiz {
+	return &listProductBiz{store: store}
+}
+
+func (biz *listProductBiz) ListProduct(
+	ctx context.Context,
+	filter *modelProduct.Filter,
+	paging *common.Paging,
+) ([]modelProduct.Product, error) {
+	data, err := biz.store.ListProduct(ctx, filter, paging)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
