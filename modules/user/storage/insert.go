66e82889661f668850212d636f5c2e624d310051
votadung <dung.vo@vtl-vtl.com>
2024-03-05 11:54:28 +0700

    add sql storage file, add insert storage file

---
+++ modules/user/storage/insert.go
@@ -0,0 +1,21 @@
+package storageUser
+
+import (
+	"context"
+	"fastFood/common"
+	modelUser "fastFood/modules/user/model"
+
+	"gorm.io/gorm"
+)
+
+func (s *sqlStorage) InsertUser(ctx context.Context, data *modelUser.UserCreate) error {
+	if err := s.db.Create(data).Error; err != nil {
+		if err != gorm.ErrRecordNotFound {
+			return common.RecordNoFound
+		}
+
+		return common.ErrDB(err)
+	}
+
+	return nil
+}
