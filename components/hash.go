ad0cba83809fc90230b2bc14aca35ea865e3cd13
votadung <dung.vo@vtl-vtl.com>
2024-03-05 15:40:30 +0700

    remove unused logic

--- components/hash.go
+++
@@ -1,8 +0,0 @@
-package components
-
-import "golang.org/x/crypto/bcrypt"
-
-func HashPassword(password string) (string, error) {
-	bytesHash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytesHash), err
-}
--- router/router.go
+++ router/router.go
@@ -3,13 +3,11 @@ package router
 import (
 	"fastFood/common"
 	jwtProvider "fastFood/components/tokenProvider/jwt"
-	favorite "fastFood/controller/favorite"
 	"fastFood/database"
 	"fastFood/middleware"
 	ginCategory "fastFood/modules/category/transport/gin"
 	ginFavorite "fastFood/modules/favorite/transport/gin"
 	ginProduct "fastFood/modules/product/transport/gin"
-	"fastFood/modules/upload"
 	storageUser "fastFood/modules/user/storage"
 	ginUser "fastFood/modules/user/transport/gin"
 	"os"
@@ -30,37 +28,40 @@ func Router() {
 
 	router := gin.Default()
 	router.Use(middleware.Recover())
-	// router.Use(middleware.Authentication())
 	router.Static("/static", "./static")
 
 	v1 := router.Group("/v1")
+	{
+		// upload := v1.Group("/upload", middlewareAuth)
+		// {
+		// 	upload.PUT("", upload.Upload(db))
+		// }
 
-	v1.PUT("/upload", upload.Upload(db))
+		{
+			v1.POST("/login", ginUser.LoginHdl(db, tokenProvider))
+			v1.POST("/register", ginUser.RegisterHdl(db))
+			v1.GET("/profile", middlewareAuth, ginUser.ProfileUserHandler(db))
+		}
 
-	{
-		v1.POST("/login", ginUser.LoginHdl(db, tokenProvider))
-		v1.POST("/register", ginUser.RegisterHdl(db))
-		v1.GET("/profile", middlewareAuth, ginUser.ProfileUserHandler(db))
-	}
-
-	{
-		v1.POST("/category", ginCategory.CreateCategoryHandler(db))
-		v1.GET("/category", ginCategory.ListCategoryHandler(db))
-		v1.GET("/category/:id", ginCategory.FindCategoryHandler(db))
-		v1.PATCH("/category/:id", ginCategory.UpdateCategoryHandler(db))
-		v1.DELETE("/category/:id", ginCategory.DeleteCategoryHandler(db))
-	}
-	{
-		v1.POST("/product", ginProduct.CreateProductHandler(db))
-		v1.GET("/product", ginProduct.ListProductHandler(db))
-		v1.GET("/product/:id", ginProduct.FindProductHandler(db))
-		v1.PATCH("/product/:id", ginProduct.UpdateProductHandler(db))
-		v1.DELETE("/product/:id", ginProduct.DeleteProductHandler(db))
-	}
-	{
-		v1.POST("/favorite", ginFavorite.CreateFavoriteHandler(db))
-		v1.GET("/favorite", favorite.GetAllFavorites(db))
-		v1.DELETE("/favorite/:id", ginFavorite.DeleteFavoriteHandler(db))
+		{
+			v1.POST("/category", ginCategory.CreateCategoryHandler(db))
+			v1.GET("/category", ginCategory.ListCategoryHandler(db))
+			v1.GET("/category/:id", ginCategory.FindCategoryHandler(db))
+			v1.PATCH("/category/:id", ginCategory.UpdateCategoryHandler(db))
+			v1.DELETE("/category/:id", ginCategory.DeleteCategoryHandler(db))
+		}
+		{
+			v1.POST("/product", ginProduct.CreateProductHandler(db))
+			v1.GET("/product", ginProduct.ListProductHandler(db))
+			v1.GET("/product/:id", ginProduct.FindProductHandler(db))
+			v1.PATCH("/product/:id", ginProduct.UpdateProductHandler(db))
+			v1.DELETE("/product/:id", ginProduct.DeleteProductHandler(db))
+		}
+		{
+			v1.POST("/favorite", ginFavorite.CreateFavoriteHandler(db))
+			// v1.GET("/favorite", favorite.GetAllFavorites(db))
+			v1.DELETE("/favorite/:id", ginFavorite.DeleteFavoriteHandler(db))
+		}
 	}
 
 	router.Run(":3000")
