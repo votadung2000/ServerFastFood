4d424f68d03bc29630b9e2ca5bc6faa0a79e06c5
votadung <dung.vo@vtl-vtl.com>
2024-03-05 11:26:20 +0700

    add model user

--- modules/user/model/user.go
+++ modules/user/model/user.go
@@ -14,14 +14,16 @@ const (
 
 type Users struct {
 	common.SQLModel
-	Name     string `json:"name" gorm:"column:name;"`
-	UserName string `json:"user_name" gorm:"column:user_name;"`
-	PassWord string `json:"-" gorm:"column:password;"`
-	Salt     string `json:"-" gorm:"column:salt"`
-	Status   int    `json:"status" gorm:"column:status;"`
-	Phone    string `json:"phone" gorm:"column:phone"`
-	Role     int    `json:"role" gorm:"column:role"`
-	AvatarId int    `json:"-" gorm:"column:avatar_id"`
+	Name        string `json:"name" gorm:"column:name;"`
+	UserName    string `json:"user_name" gorm:"column:user_name;"`
+	PassWord    string `json:"-" gorm:"column:password;"`
+	Salt        string `json:"-" gorm:"column:salt"`
+	Status      int    `json:"status" gorm:"column:status;"`
+	PhoneNumber string `json:"phone_number" gorm:"column:phone_number"`
+	Email       string `json:"email" gorm:"column:email"`
+	Address     int    `json:"address" gorm:"column:address"`
+	Role        int    `json:"role" gorm:"column:role"`
+	AvatarId    int    `json:"-" gorm:"column:avatar_id"`
 }
 
 func (Users) TableUsers() string {
