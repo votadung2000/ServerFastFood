044ca4e1a8da032d298d97abbeb2f4d2f71bd8c6
votadung <dung.vo@vtl-vtl.com>
2024-04-09 17:27:04 +0700

    add order_item model, storage

---
+++ modules/order_item/storage/insert.go
@@ -0,0 +1,21 @@
+package storageOrderItem
+
+import (
+	"context"
+	"fastFood/common"
+	modelOrderItem "fastFood/modules/order_item/model"
+
+	"gorm.io/gorm"
+)
+
+func (s *sqlStorage) CreateOrderItem(ctx context.Context, data *modelOrderItem.CreateOrderItem) error {
+	if err := s.db.Create(data).Error; err != nil {
+		if err != gorm.ErrRecordNotFound {
+			return common.RecordNoFound
+		}
+
+		return common.ErrDB(err)
+	}
+
+	return nil
+}
