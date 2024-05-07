b64d11e2e66dccf77071aba5ea564bcc0a0d8729
votadung <dung.vo@vtl-vtl.com>
2024-05-07 11:13:19 +0700

    set up module verificaiton

---
+++ modules/verification/model/verification_error.go
@@ -0,0 +1,10 @@
+package modelVerification
+
+import (
+	"errors"
+	"fastFood/common"
+)
+
+func ErrValidateRequest(msg, key string) *common.AppError {
+	return common.ErrorResponse(errors.New(msg), msg, msg, key)
+}
