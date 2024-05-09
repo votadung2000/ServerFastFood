8b711f637c2a1c4b706cd4d9dfa695ee24aa3ac1
votadung <dung.vo@vtl-vtl.com>
2024-05-09 17:45:20 +0700

    add new logic get list favorite

--- modules/favorite/model/filter.go
+++ modules/favorite/model/filter.go
@@ -1,5 +1,7 @@
 package modelFavorite
 
 type Filter struct {
-	UserId int `json:"user_id" gorm:"column:user_id;"`
+	UserId     int `json:"user_id,omitempty" gorm:"column:user_id;"`
+	StatusPr   int `json:"status,omitempty" form:"status"`
+	CategoryId int `json:"category_id,omitempty" form:"category_id"`
 }
--- modules/favorite/storage/list.go
+++ modules/favorite/storage/list.go
@@ -19,21 +19,52 @@ func (s *sqlStorage) ListFavorite(
 
 	db := s.db.Where(cond)
 
+	db = db.Joins("LEFT JOIN products ON products.id = favorites.product_id")
+
+	if f := filter; f != nil {
+		fStatus := f.StatusPr
+		if fStatus != 0 {
+			db = db.Where("products.status = ?", fStatus)
+		} else {
+			db = db.Where("products.status = ?", modelProduct.STATUS_ACTION)
+		}
+
+		fCategoryId := f.CategoryId
+		if fCategoryId != 0 {
+			db = db.Where("products.category_id = ?", fCategoryId)
+		}
+	}
+
+	// db = db.Preload("Product", func(dbPros *gorm.DB) *gorm.DB {
+
+	// 	if f := filter; f != nil {
+	// 		fStatus := f.StatusPr
+	// 		if fStatus != 0 {
+	// 			dbPros = dbPros.Where("status = ?", fStatus)
+	// 		} else {
+	// 			dbPros = dbPros.Where("status = ?", modelProduct.STATUS_ACTION)
+	// 		}
+
+	// 		fCategoryId := f.CategoryId
+	// 		if fCategoryId != 0 {
+	// 			dbPros = dbPros.Where("category_id = ?", fCategoryId)
+	// 		}
+	// 	}
+
+	// 	dbPros = dbPros.Preload("Image")
+
+	// 	return dbPros
+	// })
+
 	if err := db.
-		Select("id").
+		Select("favorites.id").
 		Table(modelFavorite.Favorite{}.TableName()).
 		Count(&paging.Total).Error; err != nil {
 		return nil, common.ErrDB(err)
 	}
 
-	db = db.Preload("Product", func(dbPros *gorm.DB) *gorm.DB {
-		dbPros = dbPros.Preload("Image")
-		dbPros = dbPros.Where("status = ?", modelProduct.STATUS_ACTION)
-		return dbPros
-	})
-
 	if err := db.Select("*").
-		Order("id desc").
+		Order("favorites.id desc").
 		Limit(paging.Limit).
 		Offset((paging.Page - 1) * paging.Limit).
 		Find(&result).Error; err != nil {
@@ -42,6 +73,24 @@ func (s *sqlStorage) ListFavorite(
 		}
 
 		return nil, common.ErrDB(err)
+	}
+
+	for i := range result {
+		// var product modelProduct.PreloadProduct
+		// if err := db.Preload("Product").First(&product, result[i].ProductId).Error; err != nil {
+		// 	return nil, common.ErrDB(err)
+		// }
+		// result[i].Product = &product
+		var product modelProduct.PreloadProduct
+
+		if err := db.Preload("Image").Where(cond).First(&product).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				return nil, common.RecordNoFound
+			}
+			return nil, common.ErrDB(err)
+		}
+
+		result[i].Product = &product
 	}
 
 	return result, nil
