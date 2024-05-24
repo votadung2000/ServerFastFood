f37ceb7dac0078a848d02e90182dd955c764b842
votadung <dung.vo@vtl-vtl.com>
2024-05-24 15:00:01 +0700

    update docs sql

--- documents/go_fast_food_db.sql
+++ documents/go_fast_food_db.sql
@@ -1,10 +1,10 @@
 -- -------------------------------------------------------------
--- TablePlus 6.0.0(550)
+-- TablePlus 6.0.4(556)
 --
 -- https://tableplus.com/
 --
 -- Database: go_fast_food_db
--- Generation Time: 2024-05-07 16:20:39.0760
+-- Generation Time: 2024-05-24 14:59:42.9440
 -- -------------------------------------------------------------
 
 
@@ -27,6 +27,21 @@ CREATE TABLE `categories` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `delivery_address` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `status` int NOT NULL DEFAULT '1',
+  `recipient_name` varchar(100) NOT NULL,
+  `street_address` varchar(255) NOT NULL,
+  `country` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `city` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `postal_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `favorites` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -61,7 +76,7 @@ CREATE TABLE `order_item` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `orders` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -76,7 +91,7 @@ CREATE TABLE `orders` (
   `completed_at` datetime DEFAULT NULL,
   `delivery_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -89,7 +104,7 @@ CREATE TABLE `products` (
   `status` int NOT NULL DEFAULT '1',
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `quantity` int DEFAULT '0',
-  `sold` int DEFAULT NULL,
+  `sold` int DEFAULT '0',
   `featured` int NOT NULL DEFAULT '1',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -131,6 +146,9 @@ INSERT INTO `categories` (`id`, `name`, `image_id`, `status`, `created_at`, `upd
 (3, 'Sandwich', 3, 1, '2024-03-05 09:26:12', '2024-03-07 01:46:06'),
 (4, 'Fruits', 4, 1, '2024-03-05 09:26:16', '2024-03-07 01:46:15');
 
+INSERT INTO `delivery_address` (`id`, `user_id`, `status`, `recipient_name`, `street_address`, `country`, `city`, `postal_code`, `description`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'USER 1', 'Hẻm 59 Hồng Lạc Phường 10 Quận Tân Bình', 'Việt Nam', 'Thành phố Hồ Chí Minh', '72117', '', '2024-05-24 07:12:15', '2024-05-24 07:12:15');
+
 INSERT INTO `favorites` (`id`, `product_id`, `user_id`, `status`, `created_at`, `updated_at`) VALUES
 (1, 1, 1, 1, '2024-03-19 06:43:25', '2024-03-19 06:43:25'),
 (2, 2, 1, 1, '2024-03-19 07:09:59', '2024-03-19 07:09:59');
@@ -156,27 +174,24 @@ INSERT INTO `images` (`id`, `url`, `width`, `height`, `cloud_name`, `extension`,
 (18, 'static/1714970759315172000_chup-anh-chan-dung4.jpg', 1024, 682, 'local', '.jpg', '2024-05-06 11:45:59', '2024-05-06 11:45:59');
 
 INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `product_name`, `status`, `quantity`, `price`, `created_at`, `updated_at`) VALUES
-(7, 7, 1, 'Beef Burger', 1, 2, 8.00, '2024-04-11 09:50:42', '2024-04-11 09:50:42'),
-(8, 7, 3, 'Seafood Burger', 1, 1, 10.00, '2024-04-11 09:50:42', '2024-04-11 09:50:42'),
-(9, 8, 1, 'Beef Burger', 1, 2, 8.00, '2024-04-11 09:51:58', '2024-04-11 09:51:58'),
-(10, 8, 3, 'Seafood Burger', 1, 1, 10.00, '2024-04-11 09:51:58', '2024-04-11 09:51:58');
+(11, 9, 1, 'Beef Burger', 1, 2, 8.00, '2024-05-23 07:10:15', '2024-05-23 07:10:15'),
+(12, 9, 3, 'Seafood Burger', 1, 4, 10.00, '2024-05-23 07:10:15', '2024-05-23 07:10:15');
 
 INSERT INTO `orders` (`id`, `user_id`, `status`, `tax_fees`, `delivery_fee`, `total`, `coupon_id`, `created_at`, `canceled_at`, `completed_at`, `delivery_at`) VALUES
-(7, 1, 1, 0.00, 0.00, 26.00, 0, '2024-04-11 09:50:42', NULL, NULL, NULL),
-(8, 1, 1, 0.00, 0.00, 26.00, 0, '2024-04-11 09:51:58', NULL, NULL, NULL);
+(9, 1, 1, 0.00, 0.00, 56.00, 0, '2024-05-23 07:10:15', NULL, NULL, NULL);
 
 INSERT INTO `products` (`id`, `name`, `image_id`, `taste`, `price`, `category_id`, `discount`, `status`, `description`, `quantity`, `sold`, `featured`, `created_at`, `updated_at`) VALUES
-(1, 'Beef Burger', 5, 'Spicy', 8, 1, 0, 1, 'Description Beef Burger', 100, NULL, 1, '2024-03-07 04:33:13', '2024-03-07 04:33:13'),
-(2, 'Chicken Burger', 6, 'Spicy', 7, 1, 0, 1, 'Description Chicken Burger', 100, NULL, 1, '2024-03-07 04:34:09', '2024-03-07 04:34:09'),
-(3, 'Seafood Burger', 7, 'Spicy', 10, 1, 0, 1, 'Description Seafood Burger', 100, NULL, 2, '2024-03-07 04:34:44', '2024-03-15 04:08:12'),
-(4, 'Double Beef Burger', 8, 'Spicy', 15, 1, 0, 1, 'Description Double Beef Burger', 50, NULL, 1, '2024-03-07 04:35:37', '2024-03-07 04:35:37'),
-(5, 'Beef Cheese Burger', 9, 'Spicy', 15, 1, 0, 1, 'Description Beef Cheese Burger', 50, NULL, 2, '2024-03-07 04:36:14', '2024-03-15 04:08:12'),
-(6, 'Mushroom Pizza', 10, 'Normal', 20, 2, 10, 1, 'Description Mushroom Pizza', 50, NULL, 2, '2024-03-18 07:57:18', '2024-03-18 08:04:13'),
-(7, 'Chicken Pizza', 11, 'Normal', 20, 2, 5, 1, 'Description Chicken Pizza', 50, NULL, 2, '2024-03-18 07:58:21', '2024-03-18 08:04:13'),
-(8, 'Strawberry', 12, 'Normal', 12, 4, 5, 1, 'Description Strawberry', 100, NULL, 2, '2024-03-18 08:00:09', '2024-03-18 08:04:13'),
-(9, 'Grape', 13, 'Normal', 12, 4, 5, 1, 'Description Grape', 100, NULL, 2, '2024-03-18 08:00:33', '2024-03-18 08:04:13'),
-(10, 'Apple', 14, 'Normal', 12, 4, 5, 1, 'Description Apple', 100, NULL, 2, '2024-03-18 08:00:56', '2024-03-18 08:04:13'),
-(11, 'Banana', 15, 'Normal', 12, 4, 5, 1, 'Description Banana', 100, NULL, 2, '2024-03-18 08:01:16', '2024-03-18 08:04:13');
+(1, 'Beef Burger', 5, 'Spicy', 8, 1, 0, 1, 'Description Beef Burger', 100, 2, 1, '2024-03-07 04:33:13', '2024-05-23 07:10:15'),
+(2, 'Chicken Burger', 6, 'Spicy', 7, 1, 0, 1, 'Description Chicken Burger', 100, 0, 1, '2024-03-07 04:34:09', '2024-05-23 04:42:54'),
+(3, 'Seafood Burger', 7, 'Spicy', 10, 1, 0, 1, 'Description Seafood Burger', 100, 4, 2, '2024-03-07 04:34:44', '2024-05-23 07:10:15'),
+(4, 'Double Beef Burger', 8, 'Spicy', 15, 1, 0, 1, 'Description Double Beef Burger', 50, 0, 1, '2024-03-07 04:35:37', '2024-05-23 04:42:54'),
+(5, 'Beef Cheese Burger', 9, 'Spicy', 15, 1, 0, 1, 'Description Beef Cheese Burger', 50, 0, 2, '2024-03-07 04:36:14', '2024-05-23 04:42:54'),
+(6, 'Mushroom Pizza', 10, 'Normal', 20, 2, 10, 1, 'Description Mushroom Pizza', 50, 0, 2, '2024-03-18 07:57:18', '2024-05-23 04:42:54'),
+(7, 'Chicken Pizza', 11, 'Normal', 20, 2, 5, 1, 'Description Chicken Pizza', 50, 0, 2, '2024-03-18 07:58:21', '2024-05-23 04:42:54'),
+(8, 'Strawberry', 12, 'Normal', 12, 4, 5, 1, 'Description Strawberry', 100, 0, 2, '2024-03-18 08:00:09', '2024-05-23 04:42:54'),
+(9, 'Grape', 13, 'Normal', 12, 4, 5, 1, 'Description Grape', 100, 0, 2, '2024-03-18 08:00:33', '2024-05-23 04:42:54'),
+(10, 'Apple', 14, 'Normal', 12, 4, 5, 1, 'Description Apple', 100, 0, 2, '2024-03-18 08:00:56', '2024-05-23 04:42:54'),
+(11, 'Banana', 15, 'Normal', 12, 4, 5, 1, 'Description Banana', 100, 0, 2, '2024-03-18 08:01:16', '2024-05-23 04:42:54');
 
 INSERT INTO `users` (`id`, `name`, `user_name`, `password`, `salt`, `phone_number`, `email`, `status`, `address`, `role`, `avatar_id`, `created_at`, `updated_at`) VALUES
 (1, 'User 1 updated', 'register1', 'e47cc3f5a5d88a7719f6a06408dc37f1', 'JlMOCUofftIAiGnqnGUIpGrEOWHCfmVQKbEOaKdSTlfqraxdCv', '0987654321', 'register1@gmail.com', 1, '123 demo user 11', 1, 18, '2024-03-05 13:53:22', '2024-05-06 04:45:59'),
